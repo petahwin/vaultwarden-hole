153fa30a3652e7ffdc9bf62a1a419bcce71e48a0
Peter Nguyen <peter.nguyen.117@gmail.com>
2023-04-29 20:29:46 -0700

    Improve robustness of export script

--- scripts/export.sh
+++ scripts/export.sh
@@ -3,13 +3,16 @@
 set -euo pipefail
 
 SCRIPT_PATH=$(dirname $(realpath -s $0))
-RECIPIENT="${RECIPIENT}"
 DATE="$(date +%Y%m%d)"
 BACKUP_FILE_PATH="/tmp/${DATE}_backup.asc"
 BW_SERVER_DATA_PATH="/opt/bitwarden/bw-data"
 DATA_DIR_NAME=$(basename $BW_SERVER_DATA_PATH)
-SERVICE_ACCOUNT_KEYPATH="${SERVICE_ACCOUNT_PATH}"
-GDRIVE_FOLDER_ID="${FOLDER_ID}"
+
+# Hide these variables in source control
+PUBKEY_PATH=""
+RECIPIENT=""
+SERVICE_ACCOUNT_KEYPATH=""
+GDRIVE_FOLDER_ID=""
 
 # # Pull encrypted data from bitwarden server
 # Export the encrypted data that doesn't require access to master password
@@ -29,6 +32,9 @@ rm -r /tmp/${DATA_DIR_NAME}
 
 # Encrypt the data again (with gpg key) and write to file
 echo "Backing up data to '${BACKUP_FILE_PATH}'"
+GNUPGHOME=$(mktemp -d)
+export GNUPGHOME
+gpg --import "${PUBKEY_PATH}"
 echo "${CONTENTS}" | base64 -d | \
  gpg \
  --encrypt \
@@ -36,9 +42,16 @@ echo "${CONTENTS}" | base64 -d | \
  --trust-model always \
  --recipient "${RECIPIENT}" \
 > "${BACKUP_FILE_PATH}"
+rm -r $GNUPGHOME
 
 # Upload to Google Drive
-source ${SCRIPT_PATH}/.venv/bin/activate
+if [[ ! -d ${SCRIPT_PATH}/.venv ]]; then
+    python3 -m venv ${SCRIPT_PATH}/.venv
+    source ${SCRIPT_PATH}/.venv/bin/activate
+    python3 -m pip install -r ${SCRIPT_PATH}/requirements.txt
+else
+    source ${SCRIPT_PATH}/.venv/bin/activate
+fi
 echo "Uploading to Google Drive..."
 python3 ${SCRIPT_PATH}/upload.py ${SERVICE_ACCOUNT_KEYPATH} ${BACKUP_FILE_PATH} ${GDRIVE_FOLDER_ID}
 echo "Done"
